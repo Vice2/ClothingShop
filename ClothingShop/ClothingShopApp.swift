0bfecc844a808cf8448df78c17b04eb8b465a4c0
Bashar <bashar.albagdadi@gmail.com>
2023-03-20 09:29:16 +0100

    Initial Commit

---
+++ ClothingShop/ClothingShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingShopApp.swift
+//  ClothingShop
+//
+//  Created by Bashar on 2023-03-20.
+//
+
+import SwiftUI
+
+@main
+struct ClothingShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
